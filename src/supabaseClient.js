6dd6fc4e7a032bb8621fab812c13436de66683f9
badrach76 <119729052+badrach76@users.noreply.github.com>
2026-08-30 02:14:03 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://YOUR_SUPABASE_PROJECT_URL.supabase.co'
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY'
+const SUPABASE_URL = 'sb_publishable_HnxeYpiIqU7BXYJ5EfCTtQ_Xtom-SXC'
+const SUPABASE_ANON_KEY = 'sb_secret_Eajp8
+••••••••••••••••-SXC-SXC'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
