efd1f70351fbf13872c2862fe2f7be1baa813afd
badrach76 <119729052+badrach76@users.noreply.github.com>
2026-08-30 18:50:52 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'sb_publishable_HnxeYpiIqU7BXYJ5EfCTtQ_Xtom-SXC'
-const SUPABASE_ANON_KEY = 'sb_secret_Eajp8
-••••••••••••••••-SXC-SXC'
+// قراءة المفاتيح بأمان من إعدادات البيئة في Vercel
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
